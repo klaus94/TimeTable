42f890ddbdd897195f3eb61e2ed51451bd8b7532
klaus <supermagic999@web.de>
2015-10-12 18:09:57 +0200

    cleaned up another time :P

--- Lecture.java
+++
@@ -1,9 +0,0 @@
-public class Lecture extends Course
-{
-	// place for additional fields
-
-	public Lecture(String moduleName, Time time, Place place, String instructor)
-	{
-		super(moduleName, time, place, instructor);
-	}
-}

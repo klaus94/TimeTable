c0c3222379480ba458b7b1ceeae10243349672e8
Tilo Werdin <werdin.tilo@arcor.de>
2015-10-19 17:47:36 +0200

    merged all the things
    looks great
    need to look where to link the views

--- src/Main.java
+++ src/Main.java
@@ -49,19 +49,19 @@ public class Main extends Application
 //		
 //		TimeTableView root = new TimeTableView(timetable);
 		
-		//AllTimeTablesView root = new AllTimeTablesView();
+		AllTimeTablesView root = new AllTimeTablesView();
 		
-		//Scene scene = new Scene(root);
+		Scene scene = new Scene(root);
 		
 
-		String filePath = "Views" + File.separator + "MainPage.fxml"; 
-		FXMLLoader loader = new FXMLLoader(Main.class.getResource(filePath));
-		FlowPane root = (FlowPane) loader.load();
-		 ViewMain controller = loader.<ViewMain>getController();
+//		String filePath = "Views" + File.separator + "MainPage.fxml"; 
+//		FXMLLoader loader = new FXMLLoader(Main.class.getResource(filePath));
+//		FlowPane root = (FlowPane) loader.load();
+//		 ViewMain controller = loader.<ViewMain>getController();
 		 
-		controller.initData("Pfosten");
+//		controller.initData("Pfosten");
 		
-		 Scene scene = new Scene(root);
+//		 Scene scene = new Scene(root);
 		 primaryStage.setScene(scene);
 		 primaryStage.setTitle("Main");
 		 primaryStage.show();

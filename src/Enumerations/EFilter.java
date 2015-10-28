e99ac873e79a037746a8962c3456979f1b06042b
Tilo Werdin <werdin.tilo@arcor.de>
2015-10-28 19:50:12 +0100

    added visibility of checkboxes and labels

--- src/Enumerations/EFilter.java
+++ src/Enumerations/EFilter.java
@@ -1,17 +1,39 @@
 package Enumerations;
 
 public enum EFilter {
-	BYMORNIGTIME,
+	BYMORNINGTIME,
 	BYAFTERNOONTIME,
 	BYMINNUMBER,
 	BYMAXNUMBER,
 	BYMAXINROW;
 	
-	public int toInt() {
+	public Boolean needDays() {
 		switch(this) {
-			case BYMORNIGTIME: return 1;
-			case BYAFTERNOONTIME: return 1;
-			default: return 0;
+			case BYMORNINGTIME: return true;
+			case BYAFTERNOONTIME: return true;
+			default: return false;
+		}
+	}
+	
+	public String toString() {
+		switch (this) {
+			case BYMORNINGTIME: return "by morningtime";
+			case BYAFTERNOONTIME: return "by afternoontime";
+			case BYMINNUMBER: return "by minimum number of courses on a day";
+			case BYMAXNUMBER: return "by maximum number of courses on a day";
+			case BYMAXINROW: return "by maximum courses in a row before a break";
+			default: return "";
+		}
+	}
+	
+	public static EFilter toEnum(String type) {
+		switch (type) {
+			case "by morningtime": return BYMORNINGTIME; 
+			case "by afternoontime": return BYAFTERNOONTIME;
+			case "by minimum number of courses on a day": return BYMINNUMBER;
+			case "by maximum number of courses on a day": return BYMAXNUMBER;
+			case "by maximum courses in a row before a break": return BYMAXINROW;
+			default: return null;
 		}
 	}
 }
--- src/ViewModel/FilterViewModel.java
+++ src/ViewModel/FilterViewModel.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import Enumerations.EFilter;
 import Model.FilterObject;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,17 +19,60 @@ import javafx.stage.Stage;
 public class FilterViewModel implements Initializable {
 	
 	@FXML private javafx.scene.control.Button btnClose;
-	
 	@FXML private javafx.scene.control.Button btnSave;
-	
 	@FXML private javafx.scene.control.TextField txtWert;
+	@FXML private javafx.scene.control.ComboBox<String> cbFilter;
+	
+	@FXML private javafx.scene.control.CheckBox chbMo;
+	@FXML private javafx.scene.control.CheckBox chbDi;
+	@FXML private javafx.scene.control.CheckBox chbMi;
+	@FXML private javafx.scene.control.CheckBox chbDo;
+	@FXML private javafx.scene.control.CheckBox chbFr;
+	
+	@FXML private javafx.scene.control.Label lblMo;
+	@FXML private javafx.scene.control.Label lblDi;
+	@FXML private javafx.scene.control.Label lblMi;
+	@FXML private javafx.scene.control.Label lblDo;
+	@FXML private javafx.scene.control.Label lblFr;
 	
 	@FXML private void btnSaveClick(ActionEvent event){
 		txtWert.setText("No");
+		if (chbMo != null) {
+			chbMo.setVisible(false);
+		}
 	}
 
 	@FXML private void btnCloseClick(ActionEvent event) {
 		((Stage) btnClose.getScene().getWindow()).close();
+	}
+	
+	@FXML private void cbFilterChange(ActionEvent event) {
+		System.out.println("hallo");
+		if (EFilter.toEnum(cbFilter.getValue()).needDays()) {
+			chbMo.setVisible(true);
+			chbDi.setVisible(true);
+			chbMi.setVisible(true);
+			chbDo.setVisible(true);
+			chbFr.setVisible(true);
+			lblMo.setVisible(true);
+			lblDi.setVisible(true);
+			lblMi.setVisible(true);
+			lblDo.setVisible(true);
+			lblFr.setVisible(true);
+			
+		} else {
+			chbMo.setVisible(false);
+			chbDi.setVisible(false);
+			chbMi.setVisible(false);
+			chbDo.setVisible(false);
+			chbFr.setVisible(false);
+			lblMo.setVisible(false);
+			lblDi.setVisible(false);
+			lblMi.setVisible(false);
+			lblDo.setVisible(false);
+			lblFr.setVisible(false);
+			
+		}
 	}
 	
 	public void initData(FilterObject filter) throws IOException{
@@ -46,21 +91,31 @@ public class FilterViewModel implements Initializable {
 			Stage stage = new Stage();
 			stage.setScene(scene);
 			stage.show();
-			
-			if(filter != null){
-				txtWert.setPromptText("" + filter.getParameter());
 				
-			}
-	
-				System.out.println("hallo1");
-				//btnClose.setText("hallo");
-				System.out.println("hallo2");
 			
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
+		chbMo.setVisible(false);
+		chbDi.setVisible(false);
+		chbMi.setVisible(false);
+		chbDo.setVisible(false);
+		chbFr.setVisible(false);
+		lblMo.setVisible(false);
+		lblDi.setVisible(false);
+		lblMi.setVisible(false);
+		lblDo.setVisible(false);
+		lblFr.setVisible(false);
+		
+		ObservableList<String> items = cbFilter.getItems();
+		items.clear();
+		items.add(EFilter.BYMORNINGTIME.toString());
+		items.add(EFilter.BYAFTERNOONTIME.toString());
+		items.add(EFilter.BYMINNUMBER.toString());
+		items.add(EFilter.BYMAXNUMBER.toString());
+		items.add(EFilter.BYMAXINROW.toString());
 		
 	}
 }
